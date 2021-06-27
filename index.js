edf26aa1e044b88fae9284d27691e597b104e6d4
mkurmarc <51379562+mkurmarc@users.noreply.github.com>
2021-06-27 01:10:17 -0700

    added games.ejs to show off react games, current templates complete, added 3 restful routes for the index, about, games files

--- index.js
+++ index.js
@@ -12,11 +12,18 @@ app.use(methodOverride('_method'));
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-
-
-
-
-
+// --- INDEX - renders index.ejs (aka home) --- //
+app.get('/', (req, res) => {
+    res.render('index'); // aka view/index.ejs
+})
+// --- ABOUT - renders about.ejs --- //
+app.get('/about', (req, res) => {
+    res.render('about'); // aka view/about.ejs
+})
+// --- GAMES - renders games.ejs --- //
+app.get('/games', (req, res) => {
+    res.render('games'); // aka view/games.ejs
+})
 
 
 
